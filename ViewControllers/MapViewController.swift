b7fae4319350313f0774edf71ea4db1212c765e1
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-04 12:35:53 +0100

    MapViewController setupUI function with searchTextField & mapView

---
+++ ViewControllers/MapViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Scene
+//
+//  Created by Shaquille McGregor on 04/09/2024.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        // map.showUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = UIColor.white
+        searchTextField.placeholder = "Search"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpUI()
+    }
+    
+    private func setUpUI() {
+        
+        view.addSubview(mapView)
+        
+        // add constraints to the mapView
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
+
