476bc45f2b0b93a54e3b959de4774a201f4668c6
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-02 13:48:44 +0000

    LocationSearchBarView added

---
+++ Subviews/LocationSearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchBarView.swift
+//  Scene
+//
+//  Created by Shaquille McGregor on 02/12/2024.
+//
+
+import SwiftUI
+
+struct LocationSearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchBarView()
+}
